c3f81b37ff052c14871b7f314de45593231024f2
Al Ameen <sayedalameen1981@gmail.com>
2026-06-09 15:53:07 +0530

    new anltcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import ValentineCard from "./components/ValentineCard";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
